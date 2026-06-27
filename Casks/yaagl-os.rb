60f03f07892ac852edc76e2c1e085b5535d0d7d5
Xingyu Wang <xander@mac.mynetworksettings.com>
2026-06-27 15:54:32 -0400

    Add yaagl and yaagl-os casks

---
+++ Casks/yaagl-os.rb
@@ -0,0 +1,25 @@
+cask "yaagl-os" do
+  version "0.3.18"
+  sha256 "096aec685ddd0001f91b1d6228581b9054c01598c39d017f0b8de8bec1c46be7"
+
+  url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.OS.app.tar.gz"
+  name "Yaagl OS"
+  desc "Yet another anime game launcher OS Version"
+  homepage "https://github.com/yaagl/yet-another-anime-game-launcher/"
+
+  livecheck do
+    url :url
+  end
+
+  auto_updates true
+  depends_on macos: :catalina
+
+  app "Yaagl OS.app"
+
+  zap trash: [
+    "~/Library/Application Support/Yaagl OS",
+    "~/Library/Caches/com.3shain.yaagl.os",
+    "~/Library/Saved Application State/com.3shain.yaagl.os.savedState",
+    "~/Library/WebKit/com.3shain.yaagl.os",
+  ]
+end
---
+++ Casks/yaagl.rb
@@ -0,0 +1,25 @@
+cask "yaagl" do
+  version "0.3.18"
+  sha256 "5b6d8d96c445bfd1f905df57c0061153265117032b1f2ef19facbab0ad094c30"
+
+  url "https://github.com/yaagl/yet-another-anime-game-launcher/releases/download/#{version}/Yaagl.app.tar.gz"
+  name "Yaagl"
+  desc "Yet another anime game launcher"
+  homepage "https://github.com/yaagl/yet-another-anime-game-launcher/"
+
+  livecheck do
+    url :url
+  end
+
+  auto_updates true
+  depends_on macos: :catalina
+
+  app "Yaagl.app"
+
+  zap trash: [
+    "~/Library/Application Support/Yaagl",
+    "~/Library/Caches/com.3shain.yaagl",
+    "~/Library/Saved Application State/com.3shain.yaagl.savedState",
+    "~/Library/WebKit/com.3shain.yaagl",
+  ]
+end
